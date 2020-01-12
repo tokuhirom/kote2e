f1c9f86f4a994d3ec4452874c66fd0f7684bf23b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-01-12 10:03:02 +0900

    optiimize

--- src/test/kotlin/kote2e/matcher/FuzzyMatcherTest.kt
+++ src/test/kotlin/kote2e/matcher/FuzzyMatcherTest.kt
@@ -2,10 +2,8 @@ package kote2e.matcher
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
 
 
 internal class FuzzyMatcherTest {
