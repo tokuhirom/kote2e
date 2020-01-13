6ff29a212c217826e8570178e70fdb15a132686e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-01-14 08:44:48 +0900

    clean up code

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("test"))
     implementation(kotlin("test-junit5"))
-    implementation("com.squareup.okhttp3:okhttp:4.3.1")
+
     implementation("com.fasterxml.jackson.core:jackson-databind:2.10.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
     implementation("com.jayway.jsonpath:json-path:2.4.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.3.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("org.slf4j:jul-to-slf4j:1.7.30")
     implementation("org.slf4j:slf4j-api:1.7.30")
